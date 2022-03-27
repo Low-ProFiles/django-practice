1bc8f1a82d09191772a434c084529b2c66f573c7
Low-profile <maxandy@naver.com>
2022-03-27 22:34:48 +0900

    Delete Secret Key

--- djangoproject/myproject/settings.py
+++ djangoproject/myproject/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--x9s@d9dk&ss8p9zu6t+_f3b^@20xx3sh63wzq&sd5^hj_orp5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
